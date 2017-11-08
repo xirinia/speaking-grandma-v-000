844f6d84a53f3d80e6c2ae3b8434fd7570e9ad30
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-08 02:35:30 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return"I LOVE YOU TOO PUMPKIN!"
   elsif phrase === phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
